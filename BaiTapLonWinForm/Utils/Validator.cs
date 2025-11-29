619bc8158973339287de1ab9fcca50b9c597a704
QuocDai1011 <146009504+QuocDai1011@users.noreply.github.com>
2025-11-29 23:44:22 +0700

    Initial project

---
+++ BaiTapLonWinForm/Utils/Validator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapLonWinForm.Utils
+{
+    public class Validator
+    {
+    }
+}
